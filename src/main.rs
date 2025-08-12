864e326c4661e8a11b8b73541f62bd079b262d4e
abskihDDloh <tficrghsoglpglsw@outlook.jp>
2025-08-12 21:16:21 +0900

    あからさまな間違いを修正。

--- src/main.rs
+++ src/main.rs
@@ -186,6 +186,10 @@ fn start(x_size: u32, y_size: u32, file_path: &str) -> Result<(), Box<dyn std::e
     }
 
     let maze_points = make_maze(x_size, y_size)?;
+    connect_unreachable_paths_to_main_path(&maze_points)?;
+    
+    let unreachable_paths = detect_unreachable_paths(&maze_points)?;
+    info!("Unreachable paths: {:?}", unreachable_paths);
 
     let maze_guard = maze_points.read().map_err(|_| {
         Box::new(std::io::Error::other(
@@ -199,9 +203,6 @@ fn start(x_size: u32, y_size: u32, file_path: &str) -> Result<(), Box<dyn std::e
         &full_path,
     )?;
 
-    connect_unreachable_paths_to_main_path(&maze_points)?;
-    let unreachable_paths = detect_unreachable_paths(&maze_points)?;
-    info!("Unreachable paths: {:?}", unreachable_paths);
     let res = extract_branch_merge_key_points_from_maze(&maze_points)?;
     info!("Generated branch-merge paths: {:?}", res);
 
